9ea23da6a1d22b3bb286c300e01a98879875f644
Daniel Wu <wuon@protonmail.com>
2020-01-20 21:43:00 -0400

    error handling

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,17 @@ import express from "express";
 import { applyMiddleware, applyRoutes } from "./utils";
 import routes from "./services";
 import middleware from "./middleware";
+import errorHandlers from "./middleware/errorHandlers";
+
+process.on("uncaughtException", e => {
+  console.log(e);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", e => {
+  console.log(e);
+  process.exit(1);
+});
 
 const { PORT = 3000 } = process.env;
 const router = express();
@@ -10,6 +21,7 @@ const server = http.createServer(router);
 
 applyMiddleware(middleware, router);
 applyRoutes(routes, router);
+applyMiddleware(errorHandlers, router);
 
 server.listen(PORT, () =>
   console.log(`Server is running http://localhost:${PORT}...`)
