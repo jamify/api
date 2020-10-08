d5dfda6cd43ee0ab0403f118a3bdbb63733f5cb5
Daniel Wu <daniel.wu@shopify.com>
2020-10-07 22:28:31 -0400

    fix: update channel schema

--- src/models/schema/channel.model.ts
+++ src/models/schema/channel.model.ts
@@ -3,8 +3,6 @@ import mongoose, { Schema } from 'mongoose';
 const ChannelSchema = new mongoose.Schema(
   {
     _id: String,
-    isPaused: { type: Boolean, default: true },
-    position: { type: Number, default: 0 },
     track: { type: Schema.Types.Mixed, default: {} },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
